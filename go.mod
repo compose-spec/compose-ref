943a110028e60528f6eb433d6c77fc76aec92e17
Nicolas De Loof <nicolas.deloof@gmail.com>
2020-01-28 17:28:08 +0100

    Introduce -f option and Compose file parsing
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/compose-ref/kraken
 
+require (
+	github.com/docker/compose-go v0.0.0-20200128154038-f2d87307c80c
+	github.com/urfave/cli/v2 v2.1.1
+)
+
 go 1.13
--- main.go
+++ main.go
@@ -1,6 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/docker/compose-go/loader"
+	"github.com/docker/compose-go/types"
+	"github.com/urfave/cli/v2"
+)
 
 const banner = `
  .---.
@@ -11,4 +20,42 @@ const banner = `
 
 func main() {
 	fmt.Println(banner)
+	var file string
+
+	app := &cli.App{
+		Name:  "kraken",
+		Usage: "Reference Compose Specification implementation",
+		Action: func(c *cli.Context) error {
+			fmt.Println("I'm alive!")
+
+			b, err := ioutil.ReadFile(file)
+			if err != nil {
+				return err
+			}
+			config, err := loader.ParseYAML(b)
+			if err != nil {
+				return err
+			}
+			files := []types.ConfigFile{}
+			files = append(files, types.ConfigFile{Filename: file, Config: config})
+			_, err = loader.Load(types.ConfigDetails{
+				WorkingDir:  ".",
+				ConfigFiles: files,
+			})
+			return err
+		},
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:        "file",
+				Value:       "compose.yaml",
+				Usage:       "Compose file",
+				Destination: &file,
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
