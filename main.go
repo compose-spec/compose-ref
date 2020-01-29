7ef2d79b605231efcabecdbbc1c0d13192b2611d
Nicolas De Loof <nicolas.deloof@gmail.com>
2020-01-29 08:19:15 +0100

    Initiate "up" implementation using docker API
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/docker/docker/api/types/strslice"
 	"io/ioutil"
 	"log"
 	"os"
 
 	"github.com/docker/compose-go/loader"
-	"github.com/docker/compose-go/types"
+	compose "github.com/docker/compose-go/types"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/client"
 	"github.com/urfave/cli/v2"
 )
 
@@ -25,31 +31,37 @@ func main() {
 	app := &cli.App{
 		Name:  "kraken",
 		Usage: "Reference Compose Specification implementation",
-		Action: func(c *cli.Context) error {
-			fmt.Println("I'm alive!")
-
-			b, err := ioutil.ReadFile(file)
-			if err != nil {
-				return err
-			}
-			config, err := loader.ParseYAML(b)
-			if err != nil {
-				return err
-			}
-			files := []types.ConfigFile{}
-			files = append(files, types.ConfigFile{Filename: file, Config: config})
-			_, err = loader.Load(types.ConfigDetails{
-				WorkingDir:  ".",
-				ConfigFiles: files,
-			})
-			return err
-		},
 		Flags: []cli.Flag{
 			&cli.StringFlag{
 				Name:        "file",
+				Aliases: []string{"f"},
 				Value:       "compose.yaml",
-				Usage:       "Compose file",
+				Usage:       "Load Compose file `FILE`",
 				Destination: &file,
+			},
+		},
+		Commands: []*cli.Command{
+			{
+				Name:    "up",
+				Usage:   "Create and start application services",
+				Action:  func(c *cli.Context) error {
+					config, err := load(file)
+					if err != nil {
+						return err
+					}
+					return doUp(config)
+				},
+			},
+			{
+				Name:    "down",
+				Usage:   "Stop services created by `up`",
+				Action:  func(c *cli.Context) error {
+					config, err := load(file)
+					if err != nil {
+						return err
+					}
+					return doDown(config)
+				},
 			},
 		},
 	}
@@ -59,3 +71,60 @@ func main() {
 		log.Fatal(err)
 	}
 }
+
+func doUp(config *compose.Config) error {
+	cli, err := client.NewEnvClient()
+	if err != nil {
+		return err
+	}
+	ctx := context.Background()
+	for _, s := range config.Services {
+		fmt.Printf("Creating container for service %s ... ", s.Name)
+		create, err := cli.ContainerCreate(ctx,
+			&container.Config{
+				Image: s.Image,
+				Cmd:   strslice.StrSlice(s.Command),
+				User:  s.User,
+				WorkingDir: s.WorkingDir,
+			},
+			&container.HostConfig{
+				Privileged: s.Privileged,
+			},
+			&network.NetworkingConfig{
+
+			},
+			"")
+		if err != nil {
+			return err
+		}
+		err = cli.ContainerStart(ctx, create.ID, types.ContainerStartOptions{})
+		if err != nil {
+			return err
+		}
+		fmt.Println(create.ID)
+	}
+
+	return nil
+}
+
+func doDown(config *compose.Config) error {
+	return nil
+}
+
+func load(file string) (*compose.Config, error) {
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	config, err := loader.ParseYAML(b)
+	if err != nil {
+		return nil, err
+	}
+	files := []compose.ConfigFile{}
+	files = append(files, compose.ConfigFile{Filename: file, Config: config})
+	return loader.Load(compose.ConfigDetails{
+		WorkingDir:  ".",
+		ConfigFiles: files,
+	})
+}
+
