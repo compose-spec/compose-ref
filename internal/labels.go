5770bccf8d76fe4e48f280b0f7fe5ddf0bddd2da
Guillaume Lours <guillaume.lours@docker.com>
2020-02-11 12:05:06 +0100

    Move Labels to a dedicated file into internal directory
    Signed-off-by: Guillaume Lours <guillaume.lours@docker.com>

---
+++ internal/labels.go
@@ -0,0 +1,10 @@
+package internal
+
+const (
+	labelNamespace = "io.compose-spec"
+	LabelService   = labelNamespace + ".service"
+	LabelNetwork   = labelNamespace + ".network"
+	LabelVolume    = labelNamespace + ".volume"
+	LabelProject   = labelNamespace + ".project"
+	LabelConfig    = labelNamespace + ".config"
+)
